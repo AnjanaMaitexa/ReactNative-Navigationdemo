cda620546addb4e6152b888a477cd87026c43c2e
MaiTexa <maitexaitsolutions@gmail.com>
2023-10-26 16:28:16 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  View,
+} from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import{createNativeStackNavigator}from "@react-navigation/native-stack"
+import Home from './screens/Home'
+import Details from './screens/Details';
+
+export type RootStackParamList={
+  Home:undefined,
+  Details:{product:Product}
+}
+
+const Stack=createNativeStackNavigator<RootStackParamList>()
+const App=()=>{
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen
+        name='Home'
+        component={Home}
+        options={{
+          title:"Trending Products"
+        }}
+        ></Stack.Screen>
+         <Stack.Screen
+        name='Details'
+        component={Details}
+        options={{
+          title:"Product Details"
+        }}
+        ></Stack.Screen>
+        </Stack.Navigator>
+    </NavigationContainer>
+ 
+  );
+}
+
+export default App;
